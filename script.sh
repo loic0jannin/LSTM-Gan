065de33943bfde99ffd82cb4a1e81982bf68d6b6
Loic <loic_jannin@berkeley.edu>
2024-04-20 16:09:01 -0700

    script

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=gan_training
+#SBATCH --output=gan_training_%j.out
+#SBATCH --error=gan_training_%j.err
+#SBATCH --partition=savio
+#SBATCH --account=loicjannin
+#SBATCH --qos=savio_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=10
+#SBATCH --cpus-per-task=1
+#SBATCH --time=72:00:00
+#SBATCH --mail-type=END,FAIL
+
+module load pytorch/1.6.0-py37-cuda10.1
+
+srun python -m torch.distributed.launch --nproc_per_node=10 gan.py
