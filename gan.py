9d565c81f651aa6ca3d735afee7c012a0260146a
Loic <loic_jannin@berkeley.edu>
2024-04-20 17:25:30 -0700

    qwd

--- gan.py
+++ gan.py
@@ -8,6 +8,9 @@ from torch.autograd.variable import Variable
 import torch.nn.functional as F
 torch.autograd.set_detect_anomaly(True)
 from torch.utils.data import DataLoader, TensorDataset
+
+from torch.nn.utils import spectral_norm
+
 # import torch.distributed as dist
 # from torch.nn.parallel import DistributedDataParallel
 
@@ -98,8 +101,11 @@ class Discriminator(nn.Module):
 
 
 # Create the Generator and Discriminator
-generator = Generator(input_size=1, hidden_size=16, num_layers=1)
-discriminator = Discriminator(input_size=1, hidden_size=16, num_layers=1)
+generator = Generator(input_size=1, hidden_size=16, num_layers=2)
+discriminator = Discriminator(input_size=1, hidden_size=16, num_layers=2)
+
+# Use spectral normalization in the discriminator
+discriminator = spectral_norm(discriminator)
 
 # # Wrap the models with DistributedDataParallel
 # generator = DistributedDataParallel(generator)
@@ -108,8 +114,14 @@ discriminator = Discriminator(input_size=1, hidden_size=16, num_layers=1)
 
 # Define the loss function and optimizers
 criterion = nn.BCELoss()
+
+# Define the optimizers
 optimizer_discriminator = torch.optim.Adam(discriminator.parameters(), lr=0.001)
 optimizer_generator = torch.optim.Adam(generator.parameters(), lr=0.001)
+
+# Use a learning rate scheduler
+schedulerD = torch.optim.lr_scheduler.StepLR(optimizer_discriminator, step_size=30, gamma=0.5)
+schedulerG = torch.optim.lr_scheduler.StepLR(optimizer_generator, step_size=30, gamma=0.5)
 
 
 # Initialize the CSV file
@@ -123,41 +135,18 @@ print("Training the GAN...")
 for epoch in tqdm(range(500)):
     for index, (real_data,) in enumerate(train_loader):
 
-        # Preparing the real data to train the discriminator:
-        real_data_label = torch.ones(batch_size,1)
-
-        # Preparing the fake data to train the discriminator: 
+            # Train the discriminator
+        discriminator.zero_grad()
+        output_real = discriminator(real_data)
         noise_data_set = torch.randn((batch_size, N))
         fake_data_set = generator(noise_data_set)
-        fake_data_label = torch.zeros(batch_size, 1)
-
-        # Creating the training samples set:
-        training_data_set = torch.cat((real_data, fake_data_set)).float()
-
-        # Creating the training labels set:
-        training_labels_set = torch.cat((real_data_label, fake_data_label))
-
-        # Train the discriminator:
-        discriminator.zero_grad()
-        output_discriminator = discriminator(training_data_set)
-
-        loss_discriminator = criterion(output_discriminator, training_labels_set)
-
+        output_fake = discriminator(fake_data_set.detach())
+        loss_discriminator = -torch.mean(output_real) + torch.mean(output_fake)
         loss_discriminator.backward()
         optimizer_discriminator.step()
-
-        # Train the generator more often
-        for _ in range(2):  # adjust this value to train generator more or less often
-            # Initialising the data for the generator: 
-            noise_data_set = torch.randn((batch_size, N))
-
-            # Train the generator:  
-            generator.zero_grad()
-            output_generator = generator(noise_data_set)
-            output_discriminator_generated = discriminator(output_generator)
-            loss_generator = criterion(output_discriminator_generated, real_data_label)
-            loss_generator.backward()
-            optimizer_generator.step()
+        
+        schedulerD.step()
+        schedulerG.step()
 
     # Write the progress to the CSV file
     if epoch % 10 == 0:
